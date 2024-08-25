b9c703005f08de9bea4e608dad67b29ed345f228
zimoyin <2556608754@qq.com>
2024-08-25 17:18:07 +0800

    fix15

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-//    `maven-publish`
 }
 rootProject.name = "autox_apk_builder"
+include(":autox_apk_builder")
