4b347766a50ea215f4852769ba7b432d7cf21bae
zimoyin <2556608754@qq.com>
2024-08-29 18:41:01 +0800

    图形化界面与命令行入口

--- README.md
+++ README.md
@@ -1,9 +1,38 @@
 # Autox 打包工具
 Autox 是基于一个 template.apk 的模板进行打包的，可以通过修改 template.apk 来实现打包。
 
+## 运行
+### A. 界面运行
+```shell
+java -jar xxx.jar 
+// java -jar xxx.jar -createConfig
+// java -jar xxx.jar xxxx配置文件路径.json
+```
 
+### B. 无界面运行
+1. 无指定配置文件运行
+```shell
+# 第一次运行后，在当前文件夹生成 apk_builder_config.json 配置文件请填写它
+java -jar xxx.jar -noGui -createConfig
+```
+2. 指定配置文件运行
+```shell
+# 第一次运行后，在当前文件夹生成 apk_builder_config.json 配置文件请填写它
+java -jar xxx.jar -noGui xxxx配置文件路径.json
+```
+### C. 配置文件
+```
+assets： 存放源码，库，资源文件的地方。他是一个字符串数组。当指定后会复制指定路径下的文件到临时命令
+templateApkPath： 模板 apk 的路径。默认为 null
+startIconPath： 启动图标的绝对路径。默认为 null
+projectJson： 项目的 json 文件路径。默认为 null。如果不指定会在 assets 中搜索
+gui： 是否使用gui 界面。默认为 true
+workDir： 工作目录。默认为 临时目录
+iconPath： 项目图标的绝对路径。默认为 null
+```
 ## API
-引入依赖
+引入依赖  
+_该仓库使用LSF 托管了大文件，所以 jitpack 无法正常的构建。因此构建的项目来自于可执行库(autojsx_builder)[https://github.com/zimoyin/autojsx_builder]_
 ```kotlin
 repositories {
     mavenCentral()
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
+import groovy.xml.dom.DOMCategory.attributes
+
 plugins {
     kotlin("jvm") version "2.0.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "com.github.zimoyin"
-version = "1.0.2-SNAPSHOT"
+version = "1.0.3"
 
 repositories {
     google()
@@ -19,10 +22,12 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.17.2")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.2")
-
     implementation("org.dom4j:dom4j:2.1.4")
-
     implementation("org.apktool:apktool-cli:2.9.3")
+    implementation("com.formdev:flatlaf:3.5.1")
+//    runtimeOnly("com.formdev:flatlaf-intellij-themes:3.5.1")
+
+
 
     testImplementation(kotlin("test"))
 }
@@ -33,4 +38,17 @@ tasks.test {
 
 kotlin {
     jvmToolchain(17)
+}
+
+// 使用 shadowJar 任务替代 jar 任务生成一个包含所有依赖的可执行 JAR 文件
+tasks.shadowJar {
+    manifest {
+        attributes["Main-Class"] = "com.github.zimoyin.autox.gui.MainKt"
+    }
+}
+
+// 让 jar 任务依赖于 shadowJar
+tasks.jar {
+    dependsOn(tasks.shadowJar)
+    enabled = false
 }
